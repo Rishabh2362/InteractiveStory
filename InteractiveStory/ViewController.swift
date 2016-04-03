5797f35ec9c39db0f8b2b0ef0441a0ea3843541d
Rishabh Verma <Rishabh2362@gmail.com>
2016-04-03 19:55:02 +0530

    Initial Commit

---
+++ InteractiveStory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InteractiveStory
+//
+//  Created by RISHABH on 03/04/16.
+//  Copyright (c) 2016 SimpleApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
